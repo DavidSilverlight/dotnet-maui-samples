662919bdb0e98785f04c8ac55c836bd9480adf96
David Silverlight <XAMLGeek@outlook.com>
2022-06-12 07:32:14 -0400

    Removed seperate SQLite folder and samples

--- SQLite/SQLite2Sample/SQLite2Sample/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace SQLite2Sample;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
--- SQLite/SQLiteSample/SQLiteSample/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SQLiteSample;
-
-public partial class AppShell : Shell
-{
-    public AppShell()
-    {
-        InitializeComponent();
-    }
-}
