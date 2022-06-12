b20705a22089fd12efb47d4896297082436e7dc1
David Silverlight <XAMLGeek@outlook.com>
2022-06-12 06:09:35 -0400

    Added core project for SQLiteSample

---
+++ SQLite/SQLiteSample/SQLiteSample/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SQLiteSample;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
