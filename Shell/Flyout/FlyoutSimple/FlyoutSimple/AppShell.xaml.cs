8a561c2f591eebc8fb5170492516975ccb9a5296
David Silverlight <XAMLGeek@outlook.com>
2022-07-24 09:45:16 -0400

    Added Sample for Simple Flyout Menu

---
+++ Shell/Flyout/FlyoutSimple/FlyoutSimple/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace FlyoutSimple;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Shell/Flyout/FlyoutSimple/FlyoutSimple/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace FlyoutSimple;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
